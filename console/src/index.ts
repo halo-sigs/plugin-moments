2dc0e27ac50400f84013a2b88728bb72624dc9b0
guqing <i@guqing.email>
2023-06-06 11:12:23 +0800

    refactor: title display of moment for comment list

--- console/src/index.ts
+++ console/src/index.ts
@@ -4,6 +4,8 @@ import { definePlugin } from "@halo-dev/console-shared";
 import MomentsList from "@/views/MomentsList.vue";
 import { markRaw } from "vue";
 import MingcuteMomentsLine from "~icons/mingcute/moment-line";
+import type { Moment } from "@/types";
+import { formatDatetime } from "./utils/date";
 
 export default definePlugin({
   components: {},
@@ -33,7 +35,7 @@ export default definePlugin({
             const moment = subject as Moment;
             return {
               label: "瞬间",
-              title: moment.spec.content.raw as string,
+              title: determineMomentTitle(moment),
               externalUrl: `/moments/${moment.metadata.name}`,
               route: {
                 name: "Moments",
@@ -45,3 +47,13 @@ export default definePlugin({
     },
   },
 });
+
+const determineMomentTitle = (moment: Moment) => {
+  const title = stripHtmlTags(moment.spec.content.raw);
+  
+  return !title?.trim() ? formatDatetime(new Date(moment.spec.releaseTime || "")) : title;
+};
+
+const stripHtmlTags = (str: string) => {
+  return str?.replace(/<\/?[^>]+(>|$)/gi, "") || ""
+};
