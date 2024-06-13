48504e79b94f15ba2d542f30f4c02263a552d7e1
Ryan Wang <i@ryanc.cc>
2024-06-13 11:04:33 +0800

    fix: layout issues on low-resolution screens
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- console/tailwind.config.js
+++ console/tailwind.config.js
@@ -2,16 +2,5 @@
 module.exports = {
   prefix: "moments-",
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    container: {
-      padding: {
-        DEFAULT: "0.5rem",
-        sm: "1.5rem",
-        lg: "4rem",
-        xl: "14rem",
-        "2xl": "22rem",
-      },
-    },
-  },
   plugins: [require("@tailwindcss/aspect-ratio")],
 };
