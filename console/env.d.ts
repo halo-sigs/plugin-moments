d6f2ed758a83786513d00aa14e613bfce6d9932c
Ryan Wang <i@ryanc.cc>
2025-07-15 18:36:26 +0800

    feat: add editor extension supports (#154)
    * feat: add editor extension supports
    
    Signed-off-by: Ryan Wang <i@ryanc.cc>
    
    * Update
    
    Signed-off-by: Ryan Wang <i@ryanc.cc>
    
    ---------
    
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- console/env.d.ts
+++ console/env.d.ts
@@ -1,7 +1,18 @@
 /// <reference types="vite/client" />
 /// <reference types="unplugin-icons/types/vue" />
 
+export {};
+
 declare module "*.vue" {
   import Vue from "vue";
   export default Vue;
+}
+
+declare global {
+  interface Window {
+    enabledPlugins: {
+      name: string;
+      version: string;
+    }[];
+  }
 }
--- console/src/components/TextEditor.vue
+++ console/src/components/TextEditor.vue
@@ -2,7 +2,9 @@
 import type { useTagQueryFetchProps } from "@/composables/use-tag";
 import MomentExtensionImage from "@/extensions/images";
 import { TagsExtension } from "@/extensions/tags";
+import type { PluginModule } from "@halo-dev/console-shared";
 import {
+  Editor,
   ExtensionAudio,
   ExtensionBlockquote,
   ExtensionBold,
@@ -43,10 +45,11 @@ import {
   ExtensionVideo,
   lowlight,
   RichTextEditor,
-  useEditor,
+  type Extensions,
 } from "@halo-dev/richtext-editor";
 import type { UseQueryReturnType } from "@tanstack/vue-query";
-import { watch } from "vue";
+import { onMounted, shallowRef, watch } from "vue";
+import { useExtension } from "./composables/use-extension";
 
 const props = withDefaults(
   defineProps<{
@@ -71,81 +74,123 @@ const emit = defineEmits<{
   (event: "update:isEmpty", value: boolean | undefined): void;
 }>();
 
-const editor = useEditor({
-  content: props.raw,
-  extensions: [
-    ExtensionParagraph,
-    ExtensionBlockquote,
-    ExtensionBold,
-    ExtensionBulletList,
-    ExtensionCode,
-    ExtensionDocument,
-    ExtensionDropcursor.configure({
-      width: 2,
-      class: "dropcursor",
-      color: "skyblue",
-    }),
-    ExtensionGapcursor,
-    ExtensionHardBreak,
-    ExtensionHistory,
-    ExtensionHorizontalRule,
-    ExtensionItalic,
-    ExtensionOrderedList,
-    ExtensionStrike,
-    ExtensionText,
-    MomentExtensionImage.configure({
-      inline: true,
-      allowBase64: false,
-      HTMLAttributes: {
-        loading: "lazy",
-      },
-    }),
-    ExtensionTaskList,
-    ExtensionLink.configure({
-      autolink: true,
-      openOnClick: false,
-    }),
-    ExtensionTextAlign.configure({
-      types: ["heading", "paragraph"],
-    }),
-    ExtensionUnderline,
-    ExtensionTable.configure({
-      resizable: true,
-    }),
-    ExtensionSubscript,
-    ExtensionSuperscript,
-    ExtensionHighlight,
-    ExtensionCommands,
-    ExtensionCodeBlock.configure({
-      lowlight,
-    }),
-    ExtensionIframe,
-    ExtensionVideo,
-    ExtensionAudio,
-    ExtensionFontSize,
-    ExtensionColor,
-    ExtensionIndent,
-    ExtensionDraggable,
-    ExtensionColumns,
-    ExtensionColumn,
-    ExtensionNodeSelected,
-    ExtensionTrailingNode,
-    ExtensionPlaceholder.configure({
-      placeholder: "有什么想说的吗...",
-    }),
-    ExtensionHighlight,
-    ExtensionListKeymap,
-    TagsExtension.configure({
-      tagQueryFetch: props.tagQueryFetch,
-    }),
-  ],
-  autofocus: "end",
-  onUpdate: () => {
-    emit("update:raw", editor.value?.getHTML() + "");
-    emit("update:html", editor.value?.getHTML() + "");
-    emit("update:isEmpty", editor.value?.isEmpty);
-    emit("update", editor.value?.getHTML() + "");
-  },
+const editor = shallowRef<Editor>();
+
+const supportedPluginNames = ["editor-hyperlink-card"];
+
+const presetExtensions = [
+  ExtensionParagraph,
+  ExtensionBlockquote,
+  ExtensionBold,
+  ExtensionBulletList,
+  ExtensionCode,
+  ExtensionDocument,
+  ExtensionDropcursor.configure({
+    width: 2,
+    class: "dropcursor",
+    color: "skyblue",
+  }),
+  ExtensionGapcursor,
+  ExtensionHardBreak,
+  ExtensionHistory,
+  ExtensionHorizontalRule,
+  ExtensionItalic,
+  ExtensionOrderedList,
+  ExtensionStrike,
+  ExtensionText,
+  MomentExtensionImage.configure({
+    inline: true,
+    allowBase64: false,
+    HTMLAttributes: {
+      loading: "lazy",
+    },
+  }),
+  ExtensionTaskList,
+  ExtensionLink.configure({
+    autolink: false,
+    openOnClick: false,
+  }),
+  ExtensionTextAlign.configure({
+    types: ["heading", "paragraph"],
+  }),
+  ExtensionUnderline,
+  ExtensionTable.configure({
+    resizable: true,
+  }),
+  ExtensionSubscript,
+  ExtensionSuperscript,
+  ExtensionHighlight,
+  ExtensionCommands,
+  ExtensionCodeBlock.configure({
+    lowlight,
+  }),
+  ExtensionIframe,
+  ExtensionVideo,
+  ExtensionAudio,
+  ExtensionFontSize,
+  ExtensionColor,
+  ExtensionIndent,
+  ExtensionDraggable,
+  ExtensionColumns,
+  ExtensionColumn,
+  ExtensionNodeSelected,
+  ExtensionTrailingNode,
+  ExtensionPlaceholder.configure({
+    placeholder: "有什么想说的吗...",
+  }),
+  ExtensionHighlight,
+  ExtensionListKeymap,
+  TagsExtension.configure({
+    tagQueryFetch: props.tagQueryFetch,
+  }),
+];
+
+const { filterDuplicateExtensions } = useExtension();
+
+onMounted(async () => {
+  const enabledPlugins = window.enabledPlugins.filter((plugin) =>
+    supportedPluginNames.includes(plugin.name)
+  );
+  const enabledPluginNames = enabledPlugins.map((plugin) => plugin.name);
+  const enabledPluginModules: PluginModule[] = enabledPluginNames
+    .map((name) => {
+      if (window[name as keyof Window]) {
+        return window[name as keyof Window];
+      }
+    })
+    .filter(Boolean);
+
+  const extensionsFromPlugins: Extensions = [];
+
+  for (const pluginModule of enabledPluginModules) {
+    const callbackFunction =
+      pluginModule?.extensionPoints?.["default:editor:extension:create"];
+
+    if (typeof callbackFunction !== "function") {
+      continue;
+    }
+
+    const extensions = await callbackFunction();
+
+    extensionsFromPlugins.push(...extensions);
+  }
+
+  const extensions = filterDuplicateExtensions([
+    ...presetExtensions,
+    ...extensionsFromPlugins,
+  ]);
+
+  editor.value = new Editor({
+    content: props.raw,
+    extensions,
+    autofocus: "end",
+    onUpdate: () => {
+      emit("update:raw", editor.value?.getHTML() + "");
+      emit("update:html", editor.value?.getHTML() + "");
+      emit("update:isEmpty", editor.value?.isEmpty);
+      emit("update", editor.value?.getHTML() + "");
+    },
+  });
 });
 
 watch(
