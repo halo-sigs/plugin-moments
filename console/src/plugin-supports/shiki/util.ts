f5eb68b5cb085e7ff217cb4610096ac79541839e
Ryan Wang <i@ryanc.cc>
2026-08-18 23:22:57 +0800

    Fix editor plugin compatibility with Halo 2.26

--- console/src/plugin-supports/shiki/util.ts
+++ console/src/plugin-supports/shiki/util.ts
@@ -1,6 +1,8 @@
+import { stores } from "@halo-dev/ui-shared";
+
 export const PLUGIN_SHIKI_NAME = "shiki";
 export const PLUGIN_SHIKI_SCRIPT_URL = `/plugins/${PLUGIN_SHIKI_NAME}/assets/static/shiki-code.js`;
 
 export function hasShikiPlugin() {
-  return window.enabledPlugins.find((p) => p.name === PLUGIN_SHIKI_NAME);
+  return stores.uiPlugins().isEnabled(PLUGIN_SHIKI_NAME);
 }
